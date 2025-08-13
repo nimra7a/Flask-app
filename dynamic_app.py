88184a8bebe030d7644d24bd71bc3053851d1628
nimra7a <168336139+nimra7a@users.noreply.github.com>
2025-08-14 00:57:11 +0500

    Add file related to dynamic prompt

---
+++ dynamic_app.py
@@ -0,0 +1,50 @@
+import streamlit as st
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from langchain_core.prompts import PromptTemplate, load_prompt
+import os
+import secret
+
+os.environ["HUGGINGFACEHUB_API_TOKEN"] = secret.HuggingFaceHub_ACCESS_TOKEN
+llm = HuggingFaceEndpoint(
+  repo_id="mistralai/Mistral-7B-Instruct-v0.2",
+  task = "text-generation"
+)
+
+model = ChatHuggingFace(llm=llm)
+
+
+st.header("Research Tool")
+
+paper_input = st.selectbox(
+    "Select a research paper:",
+    ["DIRS-CLAHS: Underwater Image Enhancement",
+    "WGAN-GP for Alzheimer's MRI Classification",
+    "Deep Q-Network for LunarLander",
+    "ARIMA for Ethereum Price Forecasting"
+    ]
+)
+
+style_input = st.selectbox(
+    "Select the explanation style:",
+    ["Beginner Friendly", 
+    "Code-Oriented", 
+    "Technical", 
+    "Methametical"]
+)
+
+length_input = st.selectbox(
+    "Select summary length:",
+    ["Short (1-2 sentences)", "Medium (1-2 paragraphs)", "Long (detailed)"]
+)
+
+template = load_prompt('template.json')
+
+
+if st.button("Submit"):
+  chain = template | model
+  result = chain.invoke({
+    'paper_input':paper_input,
+    'style_input':style_input,
+    'length_input':length_input
+  })
+  st.write(result.content)
