ed7faf0484188b2037347f86d3dda0704b3fe329
nimra7a <168336139+nimra7a@users.noreply.github.com>
2025-08-14 01:34:37 +0500

    adjust the app.py

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 import streamlit as st
 from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
 import os
-import secret
 
-os.environ["HUGGINGFACEHUB_API_TOKEN"] = secret.HuggingFaceHub_ACCESS_TOKEN
+hugging_face_key = st.secrets["HuggingFaceHub_ACCESS_TOKEN"]
 llm = HuggingFaceEndpoint(
   repo_id="mistralai/Mistral-7B-Instruct-v0.2",
   task = "text-generation"
